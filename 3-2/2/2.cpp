17a003e971c9397a3c3950f8ef9a6520711ef481
2021051285 <datmoyan@hanyang.ac.kr>
2021-09-17 14:11:44 +0900

    Added: 3-2/2/2.cpp

---
+++ 3-2/2/2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+int main(void)
+{
+    std::string str;
+
+    int count = 2;
+
+    while(count--)
+    {
+        int ioi = 0;
+        int oi = 0;
+
+        std::cout << "S = ";
+        std::getline(std::cin, str);
+
+        for(size_t i = 0; i < str.length(); i++)
+        {
+            if(i + 1 < str.length())
+            {
+                if(str[i] == 'O' && str[i + 1] == 'I')
+                {
+                    oi++;
+                }
+            }
+            if(i + 2 < str.length())
+            {
+                if(str[i] == 'I' && str[i + 1] == 'O' && str[i + 2] == 'I')
+                {
+                    ioi++;
+                }
+            }
+        }
+
+        std::cout << ioi << std::endl << oi << std::endl;
+    }
+}
