4474279986aed30c320b83349f6b91b013796837
2021051285 <datmoyan@hanyang.ac.kr>
2021-09-17 14:48:03 +0900

    Update: 3-2/4/4.cpp changed func function to overloaded functions from utilizing default values

--- 3-2/4/4.cpp
+++ 3-2/4/4.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 #include <string>
+#include <cstdarg>
 
-std::string func(std::string str_1 = "Hello", std::string str_2 = "world", std::string str_3 = "")
+std::string func(void)
+{
+    return "Hello world";
+}
+
+std::string func(std::string str_1, std::string str_2)
+{
+    return str_1 + " " + str_2;
+}
+
+std::string func(std::string str_1, std::string str_2, std::string str_3)
 {
     return str_1 + " " + str_2 + " " + str_3;
 }
