b3bfd2524f70f5e19f697bd2470d37dcf437cff9
2021051285 <datmoyan@hanyang.ac.kr>
2021-09-24 13:43:59 +0900

    Added: 4-1/1/1.cpp

---
+++ 4-1/1/1.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+
+int N;
+
+void max(const int *numBox, int *max);
+void min(const int *numBox, int *min);
+void average(const int *numBox, double *ave);
+
+int main(void)
+{
+    int ma, mi;
+    double ave;
+
+    std::cin >> N;
+
+    if(N <= 0)
+    {
+        return 0;
+    }
+
+    int *numArr = new int[N];
+    for(size_t i = 0; i < N; i++)
+    {
+        std::cin >> numArr[i];
+    }
+
+    max(numArr, &ma);
+    min(numArr, &mi);
+    average(numArr, &ave);
+
+    std::cout << "Max: " << ma << std::endl;
+    std::cout << "Min: " << mi << std::endl;
+    std::cout << "Average: " << ave << std::endl;
+
+    return 0;
+}
+
+void max(const int *numBox, int *max)
+{
+    if(N <= 0)
+    {
+        return;
+    }
+
+    *max = numBox[0];
+
+    for(size_t i = 1; i < N; i++)
+    {
+        if(numBox[i] > *max)
+        {
+            *max = numBox[i];
+        }
+    }
+
+    return;
+}
+
+void min(const int *numBox, int *min)
+{
+    if(N <= 0)
+    {
+        return;
+    }
+
+    *min = numBox[0];
+
+    for(size_t i = 1; i < N; i++)
+    {
+        if(numBox[i] < *min)
+        {
+            *min = numBox[i];
+        }
+    }
+
+    return;
+}
+
+void average(const int *numBox, double *ave)
+{
+    if(N <= 0)
+    {
+        return;
+    }
+
+    int sum = 0;
+
+    for(size_t i = 0; i < N; i++)
+    {
+        sum += numBox[i];
+    }
+
+    *ave = (double)sum / N;
+
+    return;
+}
