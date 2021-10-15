d7cf872d732c7fecabc05101d109a7056a1522e8
2021051285 <datmoyan@hanyang.ac.kr>
2021-10-15 13:48:04 +0900

    Added: 7-2/1/1.cpp

---
+++ 7-2/1/1.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+
+// class declaration
+
+class Point
+{
+protected:
+    int x;
+    int y;
+
+public:
+    Point(int _x, int _y);
+    void move(int _x, int _y);
+};
+
+class ColorPoint: public Point
+{
+private:
+    std::string color;
+
+public:
+    ColorPoint(int _x, int _y, std::string _color);
+    void setPoint(int _x, int _y);
+    void setColor(std::string _color);
+    void show(void);
+};
+
+// function definition of class Point
+
+Point::Point(int _x, int _y)
+: x(_x), y(_y) { }
+
+void Point::move(int _x, int _y)
+{
+    this->x = _x;
+    this->y = _y;
+}
+
+// function definition of class ColorPoint
+
+ColorPoint::ColorPoint(int _x, int _y, std::string _color)
+: Point(_x, _y), color(_color) { }
+
+void ColorPoint::setPoint(int _x, int _y)
+{
+    this->move(_x, _y);
+}
+
+void ColorPoint::setColor(std::string _color)
+{
+    this->color = _color;
+}
+
+void ColorPoint::show(void)
+{
+    std::cout << "The point's color is " << this->color;
+    std::cout << " which is on (" << this->x << "," << this->y << ")." << std::endl;
+}
+
+int main(void)
+{
+    ColorPoint cp(5, 5, "RED");
+    cp.show();
+
+    cp.setPoint(10, 20);
+    cp.setColor("BLUE");
+    cp.show();
+
+    return 0;
+}
